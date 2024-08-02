5c65e801f583646cfec57020ba43d8c87c5cdd5c
Julia Kim <juliakimseohyeon@gmail.com>
2024-08-01 22:34:17 -0700

    Connect with backend to dynamically render data in NoteItem component

--- src/component/NoteItem.jsx
+++ src/component/NoteItem.jsx
@@ -1,41 +1,63 @@
 import iconAddPerson from "../assets/icons/icon-add-person.svg";
 import iconPin from "../assets/icons/icon-pin.svg";
 import iconDelete from "../assets/icons/icon-delete.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function NoteItem() {
   const [hover, setHover] = useState(false);
+  const [notes, setNotes] = useState([]);
+
+  /* -------------------------------------------------------------------------- */
+  /*                   Function to load all notes in database                   */
+  /* -------------------------------------------------------------------------- */
+  useEffect(() => {
+    async function getAllNotes() {
+      try {
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}`);
+        if (response) {
+          console.log(response.data);
+          setNotes(response.data);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    getAllNotes();
+  }, []);
   return (
-    <div
-      className="relative flex flex-col"
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
-    >
-      <img
-        src={iconPin}
-        className={`absolute w-16 -left-20 top-1/4 transition-all ${
-          hover ? "block" : "hidden"
-        }`}
-      />
-      <h2 className="text-3xl font-semibold text-darkgrey">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat
-        doloremque aliquam repellendus ratione molestiae quia amet?
-      </h2>
-      <div className="flex flex-row justify-between items-center">
-        <div className="flex flex-row items-center">
-          <img src={iconAddPerson} className="w-16" />
-          <p className="text-lg text-lightgrey">
-            Roger Craig and Shakeel Avadhany
-          </p>
+    <>
+      {notes.map((note) => (
+        <div
+          key={note.id}
+          className="relative flex flex-col"
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+        >
+          <img
+            src={iconPin}
+            className={`absolute w-16 -left-20 top-1/4 transition-all ${
+              hover ? "block" : "hidden"
+            }`}
+          />
+          <h2 className="text-3xl font-semibold text-darkgrey">{note.note}</h2>
+          <div className="flex flex-row justify-between items-center">
+            <div className="flex flex-row items-center">
+              <img src={iconAddPerson} className="w-16" />
+              <p className="text-lg text-lightgrey">
+                {note.collaborator ? note.collaborator : "No Collaborators"}
+              </p>
+            </div>
+            <p className="text-lightgrey">{note.timestamp}</p>
+          </div>
+          <img
+            src={iconDelete}
+            className={`absolute w-16 -right-20 top-1/4 transition-all ${
+              hover ? "block" : "hidden"
+            }`}
+          />
         </div>
-        <p className="text-lightgrey">2 mins ago</p>
-      </div>
-      <img
-        src={iconDelete}
-        className={`absolute w-16 -right-20 top-1/4 transition-all ${
-          hover ? "block" : "hidden"
-        }`}
-      />
-    </div>
+      ))}
+    </>
   );
 }

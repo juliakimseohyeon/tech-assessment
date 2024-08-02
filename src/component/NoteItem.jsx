04824ddc3e5b683e639facad79aa642922c54bb7
Julia Kim <juliakimseohyeon@gmail.com>
2024-08-02 08:45:17 -0700

    Edit NoteItem padding so pin and delete icons are clickable

--- src/component/NoteItem.jsx
+++ src/component/NoteItem.jsx
@@ -1,11 +1,13 @@
 import iconAddPerson from "../assets/icons/icon-add-person.svg";
 import iconPin from "../assets/icons/icon-pin.svg";
+import iconPinBlue from "../assets/icons/icon-pin-blue.svg";
 import iconDelete from "../assets/icons/icon-delete.svg";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function NoteItem() {
   const [hoveredNoteId, setHoveredNoteId] = useState(null);
+  const [pinnedNoteId, setPinnedNoteId] = useState(null);
   const [notes, setNotes] = useState([]);
 
   /* -------------------------------------------------------------------------- */
@@ -16,7 +18,6 @@ export default function NoteItem() {
       try {
         const response = await axios.get(`${import.meta.env.VITE_API_URL}`);
         if (response) {
-          console.log(response.data);
           setNotes(response.data);
         }
       } catch (err) {
@@ -34,25 +35,39 @@ export default function NoteItem() {
           onMouseEnter={() => setHoveredNoteId(note.id)}
           onMouseLeave={() => setHoveredNoteId(null)}
         >
-          <img
-            src={iconPin}
-            className={`absolute w-16 -left-20 top-1/4 transition-all ${
-              hoveredNoteId === note.id ? "block" : "hidden"
-            }`}
-          />
-          <h2 className="text-3xl font-semibold text-darkgrey">{note.note}</h2>
-          <div className="flex flex-row justify-between items-center">
-            <div className="flex flex-row items-center">
-              <img src={iconAddPerson} className="w-16" />
-              <p className="text-lg text-lightgrey">
-                {note.collaborator ? note.collaborator : "No Collaborators"}
-              </p>
+          {pinnedNoteId ? (
+            <img
+              src={iconPinBlue}
+              className={`absolute w-16 left-40 top-1/4 transition-all ${
+                hoveredNoteId === note.id ? "block" : "hidden"
+              }`}
+            />
+          ) : (
+            <img
+              src={iconPin}
+              className={`absolute w-16 left-40 top-1/4 transition-all ${
+                hoveredNoteId === note.id ? "block" : "hidden"
+              }`}
+              onClick={() => setPinnedNoteId(note.id)}
+            />
+          )}
+          <div className="mx-64">
+            <h2 className="text-3xl font-semibold text-darkgrey">
+              {note.note}
+            </h2>
+            <div className="flex flex-row justify-between items-center">
+              <div className="flex flex-row items-center">
+                <img src={iconAddPerson} className="w-16" />
+                <p className="text-lg text-lightgrey">
+                  {note.collaborator ? note.collaborator : "No Collaborators"}
+                </p>
+              </div>
+              <p className="text-lightgrey">{note.timestamp}</p>
             </div>
-            <p className="text-lightgrey">{note.timestamp}</p>
           </div>
           <img
             src={iconDelete}
-            className={`absolute w-16 -right-20 top-1/4 transition-all ${
+            className={`absolute w-16 right-40 top-1/4 transition-all ${
               hoveredNoteId === note.id ? "block" : "hidden"
             }`}
           />
--- src/component/NoteList.jsx
+++ src/component/NoteList.jsx
@@ -1,7 +1,7 @@
 import NoteItem from "./NoteItem";
 export default function NoteList() {
   return (
-    <div className="mx-64">
+    <div>
       <NoteItem />
     </div>
   );

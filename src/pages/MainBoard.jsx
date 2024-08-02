707cdfb3ce16e5846c437f911878d5366f1da5c3
Julia Kim <juliakimseohyeon@gmail.com>
2024-08-01 19:24:13 -0700

    Import and add icons to MainBoard page

--- src/pages/MainBoard.jsx
+++ src/pages/MainBoard.jsx
@@ -1,8 +1,15 @@
+import iconAdd from "../assets/icons/icon-add.svg";
+import iconSearch from "../assets/icons/icon-search.svg";
+
 export default function MainBoard() {
   return (
     <main>
-      <h1>Notes</h1>
+      <div>
+        <h1>Notes</h1>
+        <img src={iconAdd} />
+      </div>
       <form>
+        <img src={iconSearch} />
         <input placeholder="Search through your Notes" name="search" />
       </form>
     </main>

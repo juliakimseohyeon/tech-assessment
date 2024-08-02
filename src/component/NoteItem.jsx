f990383accce586ceedb851ac088f54f286bfa92
Julia Kim <juliakimseohyeon@gmail.com>
2024-08-01 21:45:24 -0700

    Add logic to NoteItem so pin and delete icons appear only when it is hovered

--- src/component/NoteItem.jsx
+++ src/component/NoteItem.jsx
@@ -1,10 +1,22 @@
 import iconAddPerson from "../assets/icons/icon-add-person.svg";
 import iconPin from "../assets/icons/icon-pin.svg";
+import iconDelete from "../assets/icons/icon-delete.svg";
+import { useState } from "react";
 
 export default function NoteItem() {
+  const [hover, setHover] = useState(false);
   return (
-    <div className="relative flex flex-col">
-      <img src={iconPin} className="absolute w-16" />
+    <div
+      className="relative flex flex-col"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <img
+        src={iconPin}
+        className={`absolute w-16 -left-20 top-1/4 transition-all ${
+          hover ? "block" : "hidden"
+        }`}
+      />
       <h2 className="text-3xl font-semibold text-darkgrey">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat
         doloremque aliquam repellendus ratione molestiae quia amet?
@@ -18,6 +30,12 @@ export default function NoteItem() {
         </div>
         <p className="text-lightgrey">2 mins ago</p>
       </div>
+      <img
+        src={iconDelete}
+        className={`absolute w-16 -right-20 top-1/4 transition-all ${
+          hover ? "block" : "hidden"
+        }`}
+      />
     </div>
   );
 }

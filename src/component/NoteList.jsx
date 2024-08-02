58ac27a856fab2d9c4b19cf872cc8442f647fed0
Julia Kim <juliakimseohyeon@gmail.com>
2024-08-02 13:59:17 -0700

    Reduce height of NoteList so overflow would only be visible on scroll

--- src/component/NoteList.jsx
+++ src/component/NoteList.jsx
@@ -9,7 +9,7 @@ export default function NoteList({
   setAddBtnClicked,
 }) {
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-8 h-1/2 overflow-scroll">
       <NoteItem notes={notes} setNotes={setNotes} />
       {addBtnClicked && (
         <AddNote setAddBtnClicked={setAddBtnClicked} setNotes={setNotes} />

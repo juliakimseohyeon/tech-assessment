b82aa20f28506d13bfe6cd3ba851886ae7b6227d
Julia Kim <juliakimseohyeon@gmail.com>
2024-08-01 21:19:10 -0700

    Apply colours to MainBoard text elements

--- src/pages/MainBoard.jsx
+++ src/pages/MainBoard.jsx
@@ -5,7 +5,7 @@ export default function MainBoard() {
   return (
     <main className="flex flex-col gap-8">
       <div className="mt-12 mx-64 flex flex-row items-center justify-between">
-        <h1 className="text-5xl font-bold">Notes</h1>
+        <h1 className="text-5xl font-bold text-black">Notes</h1>
         <img src={iconAdd} className="w-16" />
       </div>
       <form className="relative mx-64 flex flex-row items-center gap-4">

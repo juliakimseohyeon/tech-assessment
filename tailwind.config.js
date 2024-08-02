9bf6e10b249e28cb4eebf878fc3c948007944149
Julia Kim <juliakimseohyeon@gmail.com>
2024-08-01 21:14:13 -0700

    Add colours to Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
+    colors: {
+      black: "#4b4b4b",
+      darkgrey: "#868686",
+      lightgrey: "#e5e5e5",
+      red: "#d62222",
+      blue: "#4b89ab",
+      green: "#61eb6e",
+    },
     extend: {},
   },
   plugins: [],

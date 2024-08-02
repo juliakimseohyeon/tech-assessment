c7c017b0c171d4c430a733f4b9048678ec4f7b18
Julia Kim <juliakimseohyeon@gmail.com>
2024-08-01 21:07:42 -0700

    Apply style to MainBoard using Tailwind

--- src/pages/MainBoard.jsx
+++ src/pages/MainBoard.jsx
@@ -3,14 +3,18 @@ import iconSearch from "../assets/icons/icon-search.svg";
 
 export default function MainBoard() {
   return (
-    <main>
-      <div>
-        <h1>Notes</h1>
-        <img src={iconAdd} />
+    <main className="flex flex-col gap-8">
+      <div className="mt-12 mx-64 flex flex-row items-center justify-between">
+        <h1 className="text-5xl font-bold">Notes</h1>
+        <img src={iconAdd} className="w-16" />
       </div>
-      <form>
-        <img src={iconSearch} />
-        <input placeholder="Search through your Notes" name="search" />
+      <form className="relative mx-64 flex flex-row items-center gap-4">
+        <img src={iconSearch} className="w-12 absolute -left-20" />
+        <input
+          placeholder="Search through your Notes"
+          name="search"
+          className="w-full"
+        />
       </form>
     </main>
   );

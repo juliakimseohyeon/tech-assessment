740009c549e6002fc4b18b095d2d3cf49e962789
Julia Kim <juliakimseohyeon@gmail.com>
2024-08-07 10:20:46 -0700

    Change order of AddNote and NoteItem components

--- src/component/NoteList.jsx
+++ src/component/NoteList.jsx
@@ -9,10 +9,10 @@ export default function NoteList({
 }) {
   return (
     <div className="flex flex-col gap-8 h-1/2 overflow-scroll">
-      <NoteItem notes={notes} setNotes={setNotes} />
       {addBtnClicked && (
         <AddNote setAddBtnClicked={setAddBtnClicked} setNotes={setNotes} />
       )}
+      <NoteItem notes={notes} setNotes={setNotes} />
     </div>
   );
 }
